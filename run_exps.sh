7032e7ede3c1747c9e08d1672f8f4d2bcd629830
eoin12345 <eoin.cummins@ucdconnect.ie>
2025-12-26 15:07:36 +0000

    update

--- run_exps.sh
+++ run_exps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for model_type in crnn transformer; do
+for model_type in  transformer; do
     for train_ds in Quartets Beethoven Haydn Mozart; do
         if [ $model_type == "transformer" ]; then
             python -u train.py --ds_name $train_ds --model_type $model_type --batch_size 1 --patience 5 --attn_window 100
