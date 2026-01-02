da2e72e22a8a9789539e9b38bf95f747e3e5b06e
eoin12345 <eoin.cummins@ucdconnect.ie>
2026-01-02 23:37:15 +0000

    update

---
+++ run_exps2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for train_ds in Quartets; do
+    python -u train.py --ds_name $train_ds --model_type transformer --batch_size 1 --patience 5 --attn_window 100 --input_feature ihcogram --encoder_dropout_p 0.0 --decoder_dropout_p 0.0 --position_encoding_dropout_p 0.0
+
+    for test_ds in Beethoven; do
+        if [ $train_ds != $test_ds ]; then
+            python -u test.py --ds_name $test_ds --model_type transformer --checkpoint_path weights/transformer/$train_ds.ckpt --input_feature ihcogram
+        fi
+    done
+done
