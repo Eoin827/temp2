d4021f34f39209c2e5088986360913229b8432d6
eoin12345 <eoin.cummins@ucdconnect.ie>
2026-01-02 23:34:46 +0000

    update

--- run_exps.sh
+++ run_exps.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 
-for model_type in  transformer; do
-    for train_ds in Quartets Beethoven Haydn Mozart; do
-        if [ $model_type == "transformer" ]; then
-            python -u train.py --ds_name $train_ds --model_type $model_type --batch_size 1 --patience 5 --attn_window 100 --input_feature ihcogram
-        else
-            python -u train.py --ds_name $train_ds --model_type $model_type --batch_size 1 --patience 5
+for train_ds in Quartets Beethoven Haydn Mozart; do
+    python -u train.py --ds_name $train_ds --model_type transformer --batch_size 1 --patience 5 --attn_window 100 --input_feature ihcogram
+
+    for test_ds in Quartets Beethoven Haydn Mozart; do
+        if [ $train_ds != $test_ds ]; then
+            python -u test.py --ds_name $test_ds --model_type transformer --checkpoint_path weights/transformer/$train_ds.ckpt --input_feature ihcogram
         fi
-        for test_ds in Quartets Beethoven Haydn Mozart; do
-            if [ $train_ds != $test_ds ]; then
-                python -u test.py --ds_name $test_ds --model_type $model_type --checkpoint_path weights/$model_type/$train_ds.ckpt --input_feature ihcogram
-            fi
-        done
     done
 done
